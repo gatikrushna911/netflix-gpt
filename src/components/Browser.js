fc0a6ed61573da8806d0c169c37c774df0d100e2
gatikrushna911 <gatikrushna911@gmail.com>
2024-07-01 10:47:14 +0530

    FIrebase API integration

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 const Browser = () => {
   return (
     <div>
+      <Header />
       <h1>Browser</h1>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="absolute w-44 mx-32 text-2xl bg-gradient-to-b from-transparent font-bold text-red-950 z-20">
+    <div className="absolute w-52 px-3 py-1 h-12 text-2xl bg-gradient-to-b from-black font-bold text-red-950 z-10">
       <img
         src="https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
         alt="netflix-logo"
